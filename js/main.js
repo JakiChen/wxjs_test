a06b0bf4024e5e54238bf2327ba178d5f3902fc3
JakiChen <jakichen@live.cn>
2015-12-03 15:27:39 +0800

    ISSUE - 10

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ function WeiXinAddContact(name){
 wx.config({
     debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
     appId: 'wx7109fa9354031087', // 必填，公众号的唯一标识
-    timestamp: 60, // 必填，生成签名的时间戳
+    timestamp: 1422946870, // 必填，生成签名的时间戳
     nonceStr: 'jhukbx', // 必填，生成签名的随机串
     signature: 'd4624c36b6795d1d99dcf0547af5443d',// 必填，签名，见附录1
     jsApiList: ['chooseWXPay','getNetworkType','previewImage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
