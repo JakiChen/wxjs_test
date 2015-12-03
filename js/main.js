2647180635716010e777415badfdecc7cba0430f
JakiChen <jakichen@live.cn>
2015-12-03 14:51:36 +0800

    ISSUE － 07
    Update wx.config

--- js/main.js
+++ js/main.js
@@ -22,10 +22,10 @@ function WeiXinAddContact(name){
 wx.config({
     debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
     appId: 'wx7109fa9354031087', // 必填，公众号的唯一标识
-    timestamp: , // 必填，生成签名的时间戳
-    nonceStr: '', // 必填，生成签名的随机串
+    timestamp: 5, // 必填，生成签名的时间戳
+    nonceStr: 'jhukbx', // 必填，生成签名的随机串
     signature: '',// 必填，签名，见附录1
-    jsApiList: [] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+    jsApiList: [chooseWXPay] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
 });
 wx.ready(function () {
 	// 1 判断当前版本是否支持指定 JS 接口，支持批量判断
